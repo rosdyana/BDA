44c891cd9b3e42b89ebddd8d90e8af888afb38b4
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2017-04-19 16:39:06 +0800

    Update ui.R

--- HW2/ui.R
+++ HW2/ui.R
@@ -11,13 +11,24 @@ ui <- dashboardPage(
   ## Sidebar content
   dashboardSidebar(
     sidebarMenu(
-      sidebarSearchForm(
-        textId = "searchText",
-        buttonId = "searchButton",
-        label = "Team Search..."
+      menuItem(
+        "Leagues",
+        tabName = "",
+        icon = icon("soccer-ball-o"),
+        menuSubItem('Leagues Table',
+                    tabName = 'league_table',
+                    icon = icon('table')),
+        menuSubItem('Leagues Map',
+                    tabName = 'leagueMap',
+                    icon = icon('map')),
+        menuSubItem(
+          'Leagues Profile',
+          tabName = 'leagueProfile',
+          icon = icon('address-book')
+        )
       ),
-      menuItem("League Table", tabName = "league_table", icon = icon("table")),
       menuItem("Top Scorers", tabName = "top_scorer_ranks", icon = icon("male")),
+      menuItem("Team Profile", tabName = "team_profile", icon = icon("shield")),
       menuItem("Settings", tabName = "settings", icon = icon("cog")),
       menuItem("About", tabName = "about", icon = icon("info-circle"))
     )
@@ -62,7 +73,7 @@ ui <- dashboardPage(
                   collapsible = TRUE,
                   selectInput("homechartType",
                               "Select Type",
-                              list("Pie", "Bar")),
+                              list("Pie", "Bar", "Line")),
                   plotlyOutput("homestatsplot")
                 ),
                 box(
@@ -72,7 +83,7 @@ ui <- dashboardPage(
                   collapsible = TRUE,
                   selectInput("awaychartType",
                               "Select Type",
-                              list("Pie", "Bar")),
+                              list("Pie", "Bar", "Line")),
                   plotlyOutput("awaystatsplot")
                 )
               )),
@@ -87,7 +98,8 @@ ui <- dashboardPage(
           collapsible = TRUE,
           tags$div(
             HTML(
-              "<p>Developer : Rosdyana Kusuma</br>Email : <a href=mailto:rosdyana.kusuma@gmail.com>rosdyana.kusuma@gmail.com</a></p>"
+              "<p>Developer : Rosdyana Kusuma</br>Email : <a href=mailto:rosdyana.kusuma@gmail.com>rosdyana.kusuma@gmail.com</a></p>",
+              "<iframe src='https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Frosdyana.shinyapps.io%2FSoccerLeague%2F&layout=box_count&size=small&mobile_iframe=true&width=61&height=40&appId' width='61' height='40' style='border:none;overflow:hidden' scrolling='no' frameborder='0' allowTransparency='true'></iframe>"
             )
           )
         )
@@ -95,12 +107,35 @@ ui <- dashboardPage(
       
       tabItem(tabName = "settings",
               h2("Settings content")),
-      
       tabItem(
-        tabName = "top_scorer_ranks",
-        h2("Top Scorers content"),
-        plotOutput("topscorerplot")
-      )
+        tabName = "team_profile",
+        selectizeInput(
+          'teamProfile',
+          'Select Team',
+          choices = c("Juventus", "Barcelona"),
+          multiple = FALSE
+        )
+      ),
+      tabItem(tabName = "top_scorer_ranks",
+              fluidPage(
+                box(
+                  title = "Top Scorers Table",
+                  width = NULL,
+                  status = "primary",
+                  solidHeader = TRUE,
+                  collapsible = TRUE,
+                  selectInput(
+                    "league",
+                    "Select league",
+                    list(
+                      "England" = c("Premier League", "Championship", "League One"),
+                      "Italy" = c("Serie A", "Serie B"),
+                      "Spain" = c("Primera Division", "Liga Adelante")
+                    )
+                  ),
+                  DT::dataTableOutput("topscorertable")
+                )
+              ))
     )
   )
 )
